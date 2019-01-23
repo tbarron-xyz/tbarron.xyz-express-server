99aff749da14236630fdaa58b5e17a5bb29ebbe3
Thomas Barron <t@tbarron.xyz>
2019-01-22 17:47:30 -0700

    adding generic type parameter to sendComponentAsStringAsync and sendComponentAsStaticMarkup

--- util/sendComponentAsStaticMarkup.ts
+++ util/sendComponentAsStaticMarkup.ts
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { RequestHandler, Response } from 'express';
 
-export const sendComponentAsStaticMarkup = (c: React.ComponentClass, getProps?: () => React.ComponentProps<typeof c>) =>
+export const sendComponentAsStaticMarkup = <T1>(c: React.ComponentClass<T1>, getProps?: () => React.ComponentProps<typeof c>) =>
     ((req, res) => {
         const props = getProps ? getProps() : null;
         const ssr = ReactDOMServer.renderToStaticMarkup(React.createElement(c, props));
         sendHtmlElementWithDoctypeAndContentType(ssr, res);
     }) as RequestHandler;
 
-export const sendComponentAsStringAsync = (
-    c: React.ComponentClass,
+export const sendComponentAsStringAsync = <T1>(
+    c: React.ComponentClass<T1>,
     getPropsAsync?: ((propsCallback: (props: React.ComponentProps<typeof c>) => void) => void)
 ) =>
     ((req, res) => {
